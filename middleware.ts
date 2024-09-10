3427691d9dfa68acaa64f2d860132f380bea443e
Adrián Núñez Pedroche <anunezpedroche@gmail.com>
2024-09-10 22:39:35 +0200

    add middleware

---
+++ middleware.ts
@@ -0,0 +1,10 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(request: NextRequest) {
+	return NextResponse.next();
+}
+
+// Routes Middleware should not run on
+export const config = {
+	matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+};
